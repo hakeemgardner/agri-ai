99c53f7067fc36ae5ad7081ccf9e51c386021de9
Hakeem <hakeemgardner34@gmail.com>
2025-10-18 18:17:13 -0500

    restructure landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,11 @@
-// ...existing code...
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./Components/Header";
-import LandingPage from "./pages/LandingPage";
+import { LandingPage } from "./pages/LandingPage";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-        </Routes>
-      </BrowserRouter>
+      <LandingPage />
     </>
   );
 }
 
 export default App;
-// ...existing code...
